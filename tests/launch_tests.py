5064e0a1d81aba37c45321ed961f85584bda1ae7
Ab2nour <61651582+Ab2nour@users.noreply.github.com>
2025-04-10 17:46:37 +0200

    style: run ruff format

--- tests/launch_tests.py
+++ tests/launch_tests.py
@@ -2,10 +2,19 @@
 import xml.etree.ElementTree as ET
 
 # ---------- Constantes ----------
-NOM_FICHIER_SH = 'launch_tests.sh'
+NOM_FICHIER_SH = "launch_tests.sh"
 XML_HEADER = '?xml version="1.0" encoding="UTF-8"?'
-TAGS = ("tests", "test", "titre", "description", "entree", "sortie",
-        "stdin", XML_HEADER)
+TAGS = (
+    "tests",
+    "test",
+    "titre",
+    "description",
+    "entree",
+    "sortie",
+    "stdin",
+    XML_HEADER,
+)
+
 
 # ---------- Fonctions ----------
 def format_whitespace(s):
@@ -17,13 +26,13 @@ def format_whitespace(s):
     """
     s = s.splitlines()
 
-    s = filter(lambda x: x != '', s) # remove empty string
+    s = filter(lambda x: x != "", s)  # remove empty string
     s = list(s)
 
-    s = map(lambda x: x.strip(), s) # strip
+    s = map(lambda x: x.strip(), s)  # strip
     s = list(s)
 
-    s = '\n'.join(s)
+    s = "\n".join(s)
 
     return s
 
@@ -54,7 +63,6 @@ def escape_xml_entities(s):
     return s
 
 
-
 def unescape_xml_entities(s):
     """
     Given a string s containing XML, unescapes <, > and &.
@@ -76,56 +84,59 @@ def create_tests_list(filename):
     ---
     filename: name of the xml test file
     """
-    with open(f'tests/{filename}.xml') as fichier_test:
+    with open(f"tests/{filename}.xml") as fichier_test:
         xml_text = escape_xml_entities(fichier_test.read())
 
         root = ET.fromstring(xml_text)
 
         try:
-            titre_tests = root.findall('titre')[0].text
+            titre_tests = root.findall("titre")[0].text
         except:
-            titre_tests = ''
+            titre_tests = ""
 
         try:
-            description_tests = root.findall('description')[0].text
+            description_tests = root.findall("description")[0].text
         except:
-            description_tests = ''
+            description_tests = ""
 
         tests = []
 
-
-        for t in root.findall('test'):
+        for t in root.findall("test"):
             t_dict = {}
 
             try:
-                t_dict['titre'] = t.findall('titre')[0].text
-                t_dict['titre'] = unescape_xml_entities(t_dict['titre'])
+                t_dict["titre"] = t.findall("titre")[0].text
+                t_dict["titre"] = unescape_xml_entities(t_dict["titre"])
             except:
-                t_dict['titre'] = ''
+                t_dict["titre"] = ""
 
             try:
-                t_dict['description'] = t.findall('description')[0].text
-                t_dict['description'] = unescape_xml_entities(t_dict['description'])
+                t_dict["description"] = t.findall("description")[0].text
+                t_dict["description"] = unescape_xml_entities(t_dict["description"])
             except:
-                t_dict['description'] = ''
+                t_dict["description"] = ""
 
             try:
-                t_dict['entree'] = format_whitespace(t.findall('entree')[0].text.strip())
-                t_dict['entree'] = unescape_xml_entities(t_dict['entree'])
+                t_dict["entree"] = format_whitespace(
+                    t.findall("entree")[0].text.strip()
+                )
+                t_dict["entree"] = unescape_xml_entities(t_dict["entree"])
             except:
-                t_dict['entree'] = ''
+                t_dict["entree"] = ""
 
             try:
-                t_dict['sortie'] = format_whitespace(t.findall('sortie')[0].text.strip())
-                t_dict['sortie'] = unescape_xml_entities(t_dict['sortie'])
+                t_dict["sortie"] = format_whitespace(
+                    t.findall("sortie")[0].text.strip()
+                )
+                t_dict["sortie"] = unescape_xml_entities(t_dict["sortie"])
             except:
-                t_dict['sortie'] = ''
+                t_dict["sortie"] = ""
 
             try:
-                t_dict['stdin'] = format_whitespace(t.findall('stdin')[0].text.strip())
-                t_dict['stdin'] = unescape_xml_entities(t_dict['stdin'])
+                t_dict["stdin"] = format_whitespace(t.findall("stdin")[0].text.strip())
+                t_dict["stdin"] = unescape_xml_entities(t_dict["stdin"])
             except:
-                t_dict['stdin'] = ''
+                t_dict["stdin"] = ""
 
             tests.append(t_dict)
 
@@ -141,42 +152,44 @@ def add_tests(filename):
 
     titre_tests, description_tests, tests = create_tests_list(filename)
 
-    with open(NOM_FICHIER_SH, mode='a') as fichier_sortie:
+    with open(NOM_FICHIER_SH, mode="a") as fichier_sortie:
         fichier_sortie.write(f"echo; echo\n")
 
-        padded_title = (' ' + titre_tests + ' ')
+        padded_title = " " + titre_tests + " "
         fichier_sortie.write(f"echo '{padded_title.center(67, '-')}'\n")
-        if description_tests != '':
+        if description_tests != "":
             fichier_sortie.write(f"echo '{description_tests}'\n")
         fichier_sortie.write(f"echo\n")
 
         for test in tests:
-            titre = test['titre']
-            description = test['description']
+            titre = test["titre"]
+            description = test["description"]
 
-            entree = test['entree']
+            entree = test["entree"]
 
             # on remplace retours à la ligne par espaces
             # car MVaP n'affiche pas de retour à la ligne
-            sortie = test['sortie'].replace('\n', ' ')
+            sortie = test["sortie"].replace("\n", " ")
 
-            stdin = test['stdin']
+            stdin = test["stdin"]
 
-            if titre != '':
+            if titre != "":
                 fichier_sortie.write(f"echo '---------- {titre} ----------'\n")
-            fichier_sortie.write(f"test_expr '{entree}' '{sortie}' '{stdin}' '{description}'\n")
+            fichier_sortie.write(
+                f"test_expr '{entree}' '{sortie}' '{stdin}' '{description}'\n"
+            )
 
 
 # ---------- Code ----------
-add_tests('expr_arithmetiques')
-add_tests('booleens')
-add_tests('commentaires')
-add_tests('declarations')
-add_tests('exposants')
-add_tests('print')
-add_tests('if')
-add_tests('else_if')
-add_tests('do_while')
-add_tests('read')
-add_tests('floats')
-add_tests('tests_prof')
+add_tests("expr_arithmetiques")
+add_tests("booleens")
+add_tests("commentaires")
+add_tests("declarations")
+add_tests("exposants")
+add_tests("print")
+add_tests("if")
+add_tests("else_if")
+add_tests("do_while")
+add_tests("read")
+add_tests("floats")
+add_tests("tests_prof")
